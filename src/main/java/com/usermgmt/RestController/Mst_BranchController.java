21c86633bab43d4759b11a47c8c1bfccba77adbc
Dini Ramadhani Salsabila <diniramadhani553@gmail.com>
2022-10-05 14:22:38 +0700

    add try catch

--- src/main/java/com/usermgmt/RestController/Mst_BranchController.java
+++ src/main/java/com/usermgmt/RestController/Mst_BranchController.java
@@ -27,67 +27,91 @@ public class Mst_BranchController {
     private final Integer maxRows = 10;
 
     @PostMapping("/add")
-    public ResponseEntity<Mst_Branch> addBranch(@Valid @RequestBody Mst_BranchInsertDTO dto){
-        System.out.println("insert branch addd");
-        Mst_Branch mst_branch = mst_branchService.insertBranch(dto);
-        return new ResponseEntity<>(mst_branch, HttpStatus.CREATED);
+    public ResponseEntity<Object> addBranch(@Valid @RequestBody Mst_BranchInsertDTO dto){
+        try {
+            System.out.println("insert branch addd");
+            Mst_Branch mst_branch = mst_branchService.insertBranch(dto);
+            return new ResponseEntity<>(mst_branch, HttpStatus.CREATED);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There is a run-time error on the server.");
+
+        }
     }
 
     @GetMapping("/list")
-    public ResponseEntity<Page<Mst_Branch>> listBranch(@RequestParam(defaultValue = "1") Integer page,
+    public ResponseEntity<Object> listBranch(@RequestParam(defaultValue = "1") Integer page,
                                                        @RequestParam(defaultValue = "") String name){
 
-        Pageable pageable = PageRequest.of(page - 1, maxRows, Sort.by("id"));
+        try {
+            Pageable pageable = PageRequest.of(page - 1, maxRows, Sort.by("id"));
 
-        Page<Mst_Branch> grid = mst_branchService.getAllBranch(pageable, name);
+            Page<Mst_Branch> grid = mst_branchService.getAllBranch(pageable, name);
 
-        return new ResponseEntity<>(grid, HttpStatus.OK);
+            return new ResponseEntity<>(grid, HttpStatus.OK);
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There is a run-time error on the server.");
+
+        }
     }
 
     @PutMapping("/update")
-    public ResponseEntity<Mst_Branch> updateBranch(@RequestBody UpdateBranchDTO updateDto,
+    public ResponseEntity<Object> updateBranch(@RequestBody UpdateBranchDTO updateDto,
                                                    @RequestParam(required = true)String id){
 
         //findbyid
-        Mst_Branch branchById = mst_branchService.getBranchById(id);
+        try {
+            Mst_Branch branchById = mst_branchService.getBranchById(id);
 
-        if (branchById != null){
-            Mst_Branch branchUpdate = mst_branchService.updateBranchById( updateDto, id);
+            if (branchById != null){
+                Mst_Branch branchUpdate = mst_branchService.updateBranchById( updateDto, id);
 
-            return new ResponseEntity<>(branchUpdate, HttpStatus.ACCEPTED);
-        }else{
+                return new ResponseEntity<>(branchUpdate, HttpStatus.ACCEPTED);
+            }else{
 
-            throw new NotFoundException("Branch with Id " + id + " Not Found!");
+                throw new NotFoundException("Branch with Id " + id + " Not Found!");
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There is a run-time error on the server.");
+
         }
-
 
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<String> deleteBranch(@RequestParam(required = true) String id){
-        Mst_Branch mst_branch = mst_branchService.getBranchById(id);
-        if(mst_branch == null){
-            throw new NotFoundException("Branch with Id " + id + " Not Found!");
-        } else{
-            mst_branchService.deleteById(id);
-            return new ResponseEntity<>("Succes Delete Mst_Branch With Id " + id, HttpStatus.OK);
+    public ResponseEntity<Object> deleteBranch(@RequestParam(required = true) String id){
+        try {
+            Mst_Branch mst_branch = mst_branchService.getBranchById(id);
+            if(mst_branch == null){
+                throw new NotFoundException("Branch with Id " + id + " Not Found!");
+            } else{
+                mst_branchService.deleteById(id);
+                return new ResponseEntity<>("Succes Delete Mst_Branch With Id " + id, HttpStatus.OK);
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There is a run-time error on the server.");
+
         }
     }
 
 
 
     @GetMapping("/{branchId}")
-    public ResponseEntity<Mst_Branch> searchBranchId(@PathVariable String branchId){
+    public ResponseEntity<Object> searchBranchId(@PathVariable String branchId){
 
 
-        Mst_Branch branch = mst_branchService.getBranchById(branchId);
+        try {
 
-        if(branch == null){
-            throw new NotFoundException("Branch with Id " + branchId + " Not Found!");
-        } else{
-            return new ResponseEntity<>(branch, HttpStatus.FOUND);
+            Mst_Branch branch = mst_branchService.getBranchById(branchId);
+
+            if(branch == null){
+                throw new NotFoundException("Branch with Id " + branchId + " Not Found!");
+            } else{
+                return new ResponseEntity<>(branch, HttpStatus.FOUND);
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("There is a run-time error on the server.");
+
         }
-
     }
 
 }
